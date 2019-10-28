0b7a8202cc1de89d4498209ceddc5030c15509ea
Student <example@example.com>
2019-10-28 11:55:21 -0700

    add functionality to form in jquery and try out array to change display for user

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,32 @@
 $(document).ready(function() {
-$("form#faves").click(event)
-event.preventDefault()
+$("form#faves").submit(function(event) {
+  event.preventDefault();
+
+// $("#color").text ();
+// $("#food").text ();
+// $("#animal").text ();
 
 
+var favColor = $("input#color").val();
+var favFood = $("input#food").val();
+var favAnimal = $("input#animal").val();
+var favMovie = $("input#movie").val();
+console.log(favColor)
+
+// $("#ansColor").text (favColor);
+// $("#ansFood").text (favFood);
+// $("#ansAnimal").text (favAnimal);
 
 
+// $("#ansFavs").text ([favColor, favFood, favAnimal, favMovie]);
+
+var favAns = [favColor, favFood, favAnimal, favMovie]
+favAns.pop()
+var favAns2 = [favMovie]
+favAns2.concat([favAns])
+$("#ansFavs").text (favAns);
+$("#ansMovie").text (favAns2);
 
 
-
-
-
-});
+    });
+  });
